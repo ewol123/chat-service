83ad65ca51284c616b6bc3776674038aef4b16ee
Gyulavári Peti <gypeti23@gmail.com>
2020-06-07 21:39:06 +0200

    feat: add connectSocketIo plugin to init socket.io server

---
+++ src/plugins/connectSocketIO.ts
@@ -0,0 +1,21 @@
+
+import http from "http";
+import io from "socket.io";
+import log from "../utils/logger";
+
+import EVENT_TYPES from "../events/index";
+
+export default function connectSocketIo(server: http.Server): io.Server {
+    const ioServer = io(server);
+    
+    ioServer.on("connection", socket => {
+      for(let TYPE in EVENT_TYPES){
+        socket.on(TYPE, () => {
+          EVENT_TYPES[TYPE](socket);
+        });
+      }
+
+    });
+    log.info(`Access your app now`);
+    return ioServer;
+  }
