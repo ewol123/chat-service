bcd9a78943e0209c59225e1d828f966ecd10da50
Gyulavári Peti <gypeti23@gmail.com>
2020-06-07 21:38:45 +0200

    feat: add createServer plugin to create a http server

---
+++ src/plugins/createServer.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import http from "http";
+import { dbConnect } from "../config/database";
+import config from "../config/app";
+import log from "../utils/logger";
+
+const { port } = config;
+
+export default function createServer(): http.Server {
+  const app = express;
+  const server = http.createServer(app);
+  server.listen(port, () => {
+    log.info("Server listening at port %d", port);
+    dbConnect();
+  });
+  return server;
+}
