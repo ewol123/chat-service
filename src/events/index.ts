2f7504c0c860fa0ea3f821645479c400fce1d2ff
Gyulavári Peti <gypeti23@gmail.com>
2020-06-07 21:40:29 +0200

    feat: events index file

---
+++ src/events/index.ts
@@ -0,0 +1,11 @@
+import Disconnect from "./Disconnect";
+import Ping from "./Ping";
+import JoinRoom from "./JoinRoom";
+import CreateUser from "./CreateUser";
+
+export default {
+  disconnect: Disconnect,
+  PING: Ping,
+  JOIN_ROOM: JoinRoom,
+  CREATE_USER: CreateUser
+}
