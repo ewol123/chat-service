314d1064e0d174cdc1179c841f89c3ae197ea569
Gyulavári Peti <gypeti23@gmail.com>
2020-06-08 21:38:02 +0200

    feat: leave room event handler

---
+++ src/events/LeaveRoom.ts
@@ -0,0 +1,36 @@
+import io from "socket.io";
+import log from "../utils/logger";
+
+import { getManager } from "typeorm";
+
+import { User } from "../models/User";
+export default async function LeaveRoom(socket: io.Socket, payload) {
+  try {
+    const user = await User.findOne({
+      where: { id: payload.userIdentifier },
+      relations: ["room"]
+    });
+    if (!user) {
+      log.warn({ message: "no user found" });
+      return null;
+    }
+    const userRoom = user.room.identifier;
+    user.room = null;
+
+    await getManager().transaction(async transactionalEntityManager => {
+      await transactionalEntityManager.save(user);
+
+      socket.leave(userRoom, err => {
+        if (err) throw err;
+        socket.emit("LEFT_ROOM", {
+          roomIdentifier: null,
+          users: [],
+          messages: [],
+          isInitialized: false
+        });
+      });
+    });
+  } catch (error) {
+    log.error(error);
+  }
+}
