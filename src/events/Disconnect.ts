768b3348c66034956428071c4b2aaa42abf8cd42
Gyulavári Peti <gypeti23@gmail.com>
2020-06-11 19:12:54 +0200

    fix: we should only remove user from a room if they actually joined one

--- src/events/Disconnect.ts
+++ src/events/Disconnect.ts
@@ -20,25 +20,25 @@ export default async function Disconnect(socket: io.Socket) {
       return null;
     }
 
-    const room = await Room.findOne({
-      where: { id: user.room.id }
-    });
-    if (!room) {
-      log.warn({ message: "no user found in the room" });
-      return null;
-    }
-
-    room.users = room.users.filter(user => user.socketId !== socket.id);
-
-    await getManager().transaction(async transactionalEntityManager => {
-      await transactionalEntityManager.save(room);
-      socket.leave(room.identifier, err => {
-        if (err) throw err;
-        socket.to(room.identifier).broadcast.emit("LEFT_ROOM", {
-          users: room.users
+    if (user.room) {
+      const room = await Room.findOne({
+        where: { id: user.room.id }
+      });
+      if (!room) {
+        log.warn({ message: "no user found in the room" });
+        return null;
+      }
+      room.users = room.users.filter(user => user.socketId !== socket.id);
+      await getManager().transaction(async transactionalEntityManager => {
+        await transactionalEntityManager.save(room);
+        socket.leave(room.identifier, err => {
+          if (err) throw err;
+          socket.to(room.identifier).broadcast.emit("LEFT_ROOM", {
+            users: room.users
+          });
         });
       });
-    });
+    }
   } catch (error) {
     log.error(error);
   }
