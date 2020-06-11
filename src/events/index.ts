143b8498b10e98b90de1a482f116f1b954b7744d
Gyulavári Peti <gypeti23@gmail.com>
2020-06-11 18:43:23 +0200

    feat: add CreateMessage event to events index

--- src/events/index.ts
+++ src/events/index.ts
@@ -3,11 +3,13 @@ import Ping from "./Ping";
 import JoinRoom from "./JoinRoom";
 import CreateUser from "./CreateUser";
 import LeaveRoom from "./LeaveRoom";
+import CreateMessage from "./CreateMessage";
 
 export default {
   disconnect: Disconnect,
   PING: Ping,
   JOIN_ROOM: JoinRoom,
   CREATE_USER: CreateUser,
-  LEAVE_ROOM: LeaveRoom
+  LEAVE_ROOM: LeaveRoom,
+  CREATE_MESSAGE: CreateMessage
 }
