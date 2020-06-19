ccfe29165ba520a122aace23913546ff4acf7df0
Gyulavári Peti <gypeti23@gmail.com>
2020-06-19 17:18:04 +0200

    feat: use redis with socket.io

--- src/plugins/connectSocketIO.ts
+++ src/plugins/connectSocketIO.ts
@@ -2,12 +2,17 @@
 import http from "http";
 import io from "socket.io";
 import log from "../utils/logger";
+import redisAdapter from 'socket.io-redis';
+import config from "../config/app";
 
 import EVENT_TYPES from "../events/index";
+
+const {redisHost, redisPort} = config;
 
 export default function connectSocketIo(server: http.Server): io.Server {
     const ioServer = io(server);
     
+    ioServer.adapter(redisAdapter({ host: redisHost, port: redisPort }));
     ioServer.on("connection", socket => {
       for(let TYPE in EVENT_TYPES){
         socket.on(TYPE, (payload) => {
