a110fb44b270bad59b578d8cda42e86392343668
Gyulavári Peti <gypeti23@gmail.com>
2020-06-21 20:59:27 +0200

    fix: order imports, change log msg

--- src/plugins/connectSocketIO.ts
+++ src/plugins/connectSocketIO.ts
@@ -1,18 +1,19 @@
 
 import http from "http";
 import io from "socket.io";
-import log from "../utils/logger";
 import redisAdapter from 'socket.io-redis';
-import config from "../config/app";
+import log from "../utils/logger";
 
 import EVENT_TYPES from "../events/index";
+import config from "../config/app";
 
-const {redisHost, redisPort} = config;
+const {redisHost, redisPort} = config.redis;
 
 export default function connectSocketIo(server: http.Server): io.Server {
     const ioServer = io(server);
-    
+
     ioServer.adapter(redisAdapter({ host: redisHost, port: redisPort }));
+  
     ioServer.on("connection", socket => {
       for(let TYPE in EVENT_TYPES){
         socket.on(TYPE, (payload) => {
@@ -21,6 +22,6 @@ export default function connectSocketIo(server: http.Server): io.Server {
       }
 
     });
-    log.info(`Access your app now`);
+    log.info(`Socket.io connected to server`);
     return ioServer;
   }
